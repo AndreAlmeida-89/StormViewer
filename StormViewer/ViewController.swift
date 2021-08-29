8563295673dee7bc08bd0d1d4a48123f188d043b
AndreAlmeida-89 <andre_rdo@hotmail.com>
2021-08-29 20:26:12 -0300

    add vc to show image

--- StormViewer/ViewController.swift
+++ StormViewer/ViewController.swift
@@ -22,9 +22,7 @@ class ViewController: UITableViewController {
             if item.hasPrefix("nssl"){
                 pictures.append(item)
             }
-                
         }
-        print(pictures)
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -32,11 +30,17 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
-            cell.textLabel?.text = pictures[indexPath.row]
-            return cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
     }
 
-
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 }
 
+
